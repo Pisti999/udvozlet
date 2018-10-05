c9438e639295a54c1324a7684231252040d63c8a
pisti33333@gmail.com <>
2018-10-05 16:39:48 +0200

    Második változat külön osztállya.

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -15,7 +15,10 @@ public class Udvozlet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Üdvözöllek!");
+       Ember no = new Ember("Gabi");
+       Ember ferfi = new Ember("Zoltán");
+       
+       System.out.println(no.koszont());
+       System.out.println(ferfi.koszont());
     }
-    
 }
